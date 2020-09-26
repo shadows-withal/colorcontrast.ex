792ad6c8e236dcb65769cd1ad9d022c2fe363aed
mokou <mokou@posteo.de>
2020-09-26 22:34:09 +0200

    chore: Prepare for publishing

--- mix.exs
+++ mix.exs
@@ -6,8 +6,24 @@ defmodule ColorContrast.MixProject do
       app: :color_contrast,
       version: "1.0.0",
       elixir: "~> 1.10",
+      description: description(),
+      package: package(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: "https://github.com/thcz/colorcontrast.ex"
+    ]
+  end
+
+  defp description do
+    "A library that selects black or white depending on contrast with a chosen background color."
+  end
+
+  defp package do
+    [
+      name: "color_contrast",
+      files: ~w(lib .formatter.exs mix.exs README LICENSE),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/thcz/colorcontrast.ex"}
     ]
   end
 
